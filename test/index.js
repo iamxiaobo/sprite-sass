7bec0e1241cdd504f748eea920bafdea0ee1d222
sunxiaobo <sunxiaobo.cn@gmail.com>
2016-10-11 20:40:01 +0800

    添加babel编译

--- test/index.js
+++ test/index.js
@@ -1,19 +1,10 @@
 var spriteSass = require('../index')
 var path = require('path')
 
-/*
-function generateFiles(list) {
-  return list.map(name => path.join(__dirname, 'img', name))
-}
-*/
-
 spriteSass({
   direction: 'horizontal',
-  src: ["./**/*.png", "!./*.png"],
+  src: ["./**/*.png", "!./*.png", "!./public/**"],
   out: path.join(__dirname, 'public'),
-  // backgroundPrefix: '/',
-  // dest: path.join(__dirname, 'sprite.png'),
-  // destSass: path.join(__dirname, 'sprite.sass'),
   gap: {
     vertical: 100,
     horizontal: 100
